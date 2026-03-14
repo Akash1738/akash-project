07061ff476f3cf3efbbe57cc6f0cdec1ad2a2aab
Akash <akashbajgire17@gmail.com>
2026-03-14 13:44:46 +0530

    added terraform infrastructure

---
+++ terraform/ec2.tf
@@ -0,0 +1,9 @@
+resource "aws_instance" "devops_ec2" {
+  ami           = "ami-0b6c6ebed2801a5cb"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.devops_subnet.id
+
+  tags = {
+    Name = "devops-ec2"
+  }
+}
