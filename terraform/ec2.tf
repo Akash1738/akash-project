23d3437672d1d0b648d012f04e4415636c9bc34b
@kki... <akashbajgire12@gmail.com>
2026-03-16 12:36:52 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "devops_server" {
   vpc_security_group_ids = [aws_security_group.devops_sg.id]
 
   associate_public_ip_address = true
-  key_name = "akash.pem"
+  key_name = "akash"
 
   tags = {
     Name = "devops-server"
