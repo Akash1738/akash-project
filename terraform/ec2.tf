bc1bcca680c3c04e28ab568099f3b9cd4680b3b2
@kki... <akashbajgire12@gmail.com>
2026-03-16 12:02:05 +0530

    Update ec2.tf

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,7 +1,9 @@
 resource "aws_instance" "devops_ec2" {
   ami           = "ami-0b6c6ebed2801a5cb"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.devops_subnet.id
+  subnet_id = aws_subnet.public_subnet.id
+
+  associate_public_ip_address = true
 
   tags = {
     Name = "devops-ec2"
