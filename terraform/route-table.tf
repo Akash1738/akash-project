9a1990c0d7b5f5548732c2a8d1e4ce4021e341d7
@kki... <akashbajgire12@gmail.com>
2026-03-16 12:21:07 +0530

    Update route-table.tf

--- terraform/route-table.tf
+++ terraform/route-table.tf
@@ -1,18 +1,17 @@
 resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.devops_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.gw.id
   }
 
-
   tags = {
-    Name = "devops-route-table"
+    Name = "devops-public-rt"
   }
 }
 
 resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.devops_subnet.id
-  route_table_id = aws_route_table.devops_route_table.id
+  subnet_id = aws_subnet.public_subnet.id
+  route_table_id = aws_route_table.public_rt.id
 }
